d36bd729b6bcd267d67968c1e2f7c0c49a31aabd
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-05-03 19:21:03 -0400

    adicionando dependecias iniciais

---
+++ src/main/java/com/elastech/Elastech/ElastechApplication.java
@@ -0,0 +1,13 @@
+package com.elastech.Elastech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElastechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elastech/Elastech/ElastechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastech.Elastech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElastechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
