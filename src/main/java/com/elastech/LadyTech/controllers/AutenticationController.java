ed862227846d2d7090b07e076c14e4623bbe9615
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-05-14 14:11:08 -0400

    login

--- src/main/java/com/elastech/LadyTech/controllers/AutenticationController.java
+++ src/main/java/com/elastech/LadyTech/controllers/AutenticationController.java
@@ -8,11 +8,12 @@ import com.elastech.LadyTech.repositories.TechnicalRepository;
 import com.elastech.LadyTech.repositories.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AutenticationController {
     @Autowired
     private UserRepository userRepository;
@@ -23,22 +24,22 @@ public class AutenticationController {
     @Autowired
     private HttpSession session;
     @PostMapping("/login")
-    private String login(@RequestParam String username, @RequestParam String password){
+    public String login(@RequestParam String username, @RequestParam String password){
 
         User user = userRepository.findByUserName(username);
         if (user!= null){
             session.setAttribute("usuarioLogade", user);
-            return "redirect:/tela-usuario";
+            return "redirect:/usuario-historico";
         }
         Technical technical = technicalRepository.findByUserName(username);
         if (technical !=null){
             session.setAttribute("usuarioLogado", technical);
-            return "redirect:/tela-tecnico";
+            return "redirect:/tecnico-historico";
         }
         Administrator administrator = administratorRepository.findByUserName(username);
         if (administrator != null){
             session.setAttribute("usuarioLogado", administrator);
-            return "redirect:/cadastrouser";
+            return "redirect:/administrador-usuarios";
         }
         return "redirect:/login";
 
