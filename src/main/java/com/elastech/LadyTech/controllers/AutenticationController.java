959c75431844b6c242d1ed9dd19f33646ffba077
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-05-15 10:13:37 -0400

    correção de rotas tecnico e menu template

--- src/main/java/com/elastech/LadyTech/controllers/AutenticationController.java
+++ src/main/java/com/elastech/LadyTech/controllers/AutenticationController.java
@@ -2,6 +2,7 @@ package com.elastech.LadyTech.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.elastech.LadyTech.repositories.TechnicalRepository;
 import com.elastech.LadyTech.repositories.UserRepository;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/autentication")
@@ -27,31 +29,60 @@ public class AutenticationController {
 	private AdministratorRepository administratorRepository;
 	@Autowired
 	private HttpSession session;
+	private User usuarioLogado = new User();
+	private Technical technicalLogado = new Technical();
+	private Administrator administratorLogado = new Administrator();
+
+
+
 
 	@PostMapping("/login")
-	public String login(@RequestParam String username, @RequestParam String password) {
+	public String login(@RequestParam String username, @RequestParam String password, HttpSession session) {
 
 		User user = userRepository.findByUserName(username);
 		if (user != null && user.getPassword().equals(password)) {
 			session.setAttribute("usuarioLogado", user);
+			usuarioLogado.setName(user.getName());
+			usuarioLogado.setIdUser(user.getIdUser());
 			return "redirect:/user/consult-called";
 		}
 
 		Technical technical = technicalRepository.findByUserName(username);
 		if (technical != null && technical.getPassword().equals(password)) {
-			session.setAttribute("usuarioLogado", technical);
-			return "redirect:/tecnico-historico";
+			session.setAttribute("technicalLogado", technical);
+			technicalLogado.setName(technical.getName());
+			technicalLogado.setIdTechnical(technical.getIdTechnical());
+			return "redirect:/technical/consult-called";
 		}
 
 		Administrator administrator = administratorRepository.findByUserName(username);
 		if (administrator != null && administrator.getPassword().equals(password)) {
 			session.setAttribute("administradorLogado", administrator);
+			administratorLogado.setName(administrator.getName());
+			administratorLogado.setIdAdministrator(administrator.getIdAdministrator());
 
 			return "redirect:/called/consult-called";
 		}
 
-		return "redirect:/login";
+		return "redirect:/";
 	}
+	@GetMapping("/usuario-historico")
+	public String usuarioHistorico(Model model, HttpSession session) {
+		if(usuarioLogado.getName()!=null){
+			model.addAttribute("nome",usuarioLogado.getName());
+
+		}
+		if(technicalLogado.getName()!=null){
+			model.addAttribute("nome",technicalLogado.getName());
+		}
+		if(administratorLogado.getName()!=null){
+			model.addAttribute("nome",administratorLogado.getName());
+
+		}
+		return "usuario-historico";
+	}
+
+
 
 	@GetMapping("/logout")
 	public String logout() {
--- src/main/java/com/elastech/LadyTech/controllers/TechnicalController.java
+++ src/main/java/com/elastech/LadyTech/controllers/TechnicalController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,19 @@ public class TechnicalController {
 	@GetMapping
 	private List<Called> getAllCalled() {
 		return calledRepository.findAll();
+	}
+	@GetMapping("/consult-called")
+	private String getAllCalled(Model model) {
+		List<Called> called = calledRepository.findAll();
+		model.addAttribute("chamados", called);
+		return "tecnico-historico";
+	}
+	@GetMapping("/consult-called-open")
+	private String getCalledOpen(Model model) {
+		String status = "Aguardando Técnico";
+		List<Called> called = calledRepository.findByStatus(status);
+		model.addAttribute("chamados", called);
+		return "tecnico-chamados-abertos";
 	}
 
 
